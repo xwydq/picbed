f7b1f9e1f80339d6614a1e7c6840ba7c28dd1769
xuweiyun <xuwy@isyscore.com>
2019-05-31 10:17:43 +0800

    replace img path to url

--- sublocalpic.py
+++ sublocalpic.py
@@ -2,14 +2,30 @@
 import os
 import pandas as pd
 import re
+import shutil
 
 df_sub = pd.DataFrame()
-
+# 图床图片的本地路径
 path = 'E:\\GooglePhoto\\'
 imgtypes = ('png', 'jpg', 'webp', 'gif')
-prefix = 'https://github.com/xwydq/picbed/blob/master/'
 prefix = 'https://raw.githubusercontent.com/xwydq/picbed/master/'
 
+# Markdown 笔记文件夹
+md_odir = 'C:\\Users\\xwydq\\Google 云端硬盘\\note'
+# Markdown 笔记文件夹替换img url
+md_dir = 'C:\\Users\\xwydq\\Google 云端硬盘\\noteurl'
+
+
+# copy本地文件
+def copy_and_overwrite(from_path, to_path):
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path)
+
+copy_and_overwrite(md_odir, md_dir)
+
+
+## list all img file path
 files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
@@ -30,72 +46,65 @@ df_sub = pd.DataFrame({
     'resub_pre': resub_pre,
     'resub_after': resub_after
 })
-
 df_sub.to_csv('img_path_url_4md.csv', index=False)
 
-## 正则替换
 
+#############
+### 正则替换
 ## list md files
-md_dir = 'E:\\testPros\\note\\'
-files = []
-# r=root, d=directories, f = files
+md_files = []
 for r, d, f in os.walk(md_dir):
     for file in f:
         if file.lower().endswith('.md'):
-            files.append(os.path.join(r, file))
+            md_files.append(os.path.join(r, file))
 
 ## 按行读取单个 md 文件
 # 每行循环进行正则替换
-md_file = 'E:\\testPros\\note\\superset\\superset.md'
+for md_file in md_files:
+    with open(md_file, "r", encoding='utf-8') as f:
+        md_lines = f.readlines()
 
-with open(md_file, "r", encoding='utf-8') as f:
-    for line in f.readlines():
-        # print(line)
+    for li in range(len(md_lines)):
+        line = md_lines[li]
         for fi in range(len(file_nm)):
             fd = file_nm[fi]
-            # img_url[fi]
-        # for fd in file_nm:
             ptn = r'\!\[.*\]\(.*' + fd + r'\)'
             if re.search(ptn, line):
                 print(line)
                 substr = re.sub(r'\(.*' + fd + r'\)',
-                       r'(' + img_url[fi] + r')', line, count=0, flags=0)
-                print(substr)
+                                r'(' + img_url[fi] + r')', line, count=0, flags=0)
+                md_lines[li] = substr
+
+    with open(md_file, "w", encoding='utf-8') as f:
+        f.writelines(md_lines)
+
+
+# with open(md_file, "r", encoding='utf-8') as f:
+#     for line in f.readlines():
+#         # print(line)
+#         for fi in range(len(file_nm)):
+#             fd = file_nm[fi]
+#             # img_url[fi]
+#         # for fd in file_nm:
+#             ptn = r'\!\[.*\]\(.*' + fd + r'\)'
+#             if re.search(ptn, line):
+#                 print(line)
+#                 substr = re.sub(r'\(.*' + fd + r'\)',
+#                        r'(' + img_url[fi] + r')', line, count=0, flags=0)
+#                 print(substr)
+
+
+
+
+
 
 
 ##################
 ## 关于反斜杠在正则表达式re
-import re
-
-string = '3\8'
-m = re.search(r'(\d+\\)', string)
-print(m.group(1))
-
-re.search('\!\[.*\]\(E:\\jdfhek.jpg\)', 'jksjdj![](E:\jdfhek.jpg)')
-re.search(r'\!\[.*\]\(E:\\jdfhek.jpg\)', 'jksjdj![](E:\jdfhek.jpg)')
-
-
-
-
-r'\!\[.*\]\(' + 'E:\\jdfhek.jpg' + r'\)'
-
-
-re.search(re.compile(ptn), 'jksjdj![](jdfhek.jpg)')
-
-'\\!\\[.*\\]\\(E:\\GooglePhoto\\4f54e63f817c4293b8f71adb15b1e3f7.webp\\)'.replace(r'\\','\\')
-
-
-
-for f in files:
-    print(f)
-    print(os.path.basename(f))
-    print(prefix + os.path.basename(f))
-    file_dirnm = f
-    file_nm = os.path.basename(f)
-    img_url = prefix + os.path.basename(f)
-    df_sub = pd.DataFrame(
-        file_dirnm=file_dirnm,
-        file_nm=file_nm,
-        img_url=img_url,
-        resub_pre='![.*]\(\)'
-    )
+# import re
+# string = '3\8'
+# m = re.search(r'(\d+\\)', string)
+# print(m.group(1))
+#
+# re.search('\!\[.*\]\(E:\\jdfhek.jpg\)', 'jksjdj![](E:\jdfhek.jpg)')
+# re.search(r'\!\[.*\]\(E:\\jdfhek.jpg\)', 'jksjdj![](E:\jdfhek.jpg)')
